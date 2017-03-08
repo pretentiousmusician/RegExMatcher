02c7c95a98a992db7e8318f49503ab68c6a74394
pretentiousmusician <brendanjkerr@gmail.com>
2017-03-08 00:50:10 -0500

    Getting there!

--- RegEx.py
+++ RegEx.py
@@ -1,39 +1,35 @@
+import sys
+
 class RegEx:
 
-    def is_match(self, s, p):
-        sPass = 0
-        pPass = 0
-        sub = None
-        match = -1
-        while sPass < (len(s) - 1) and pPass < (len(p) - 1):
-            if p[pPass] == '(':
-                sub = self.get_substr(p[pPass+1])
+    def main(self):
+        exp = input("Please enter the regular expression to match to:")
+        string = input("Now please enter the string to match to it:")
+        print(self.is_submatch(string, exp))
+
+    def is_submatch(self, s, p):
+        spass = 0
+        for i in range(len(p)-1):
+            #remember edge case for final char!!!
+            if p[i] == '(':
+                sub = self.get_substr(p[1:])
                 if sub is None:
-                    return None
-                pPass += (len(sub)+2)
-                #This should appropriately move the index
-            elif p[pPass] == '*':
-                    print("Error, should have avoided this!");
-                    exit(1)
+                    return False
+                    #Or maybe continue?
+                i += (len(sub)+1)
+                #right increment?
+            elif p[i+1] == '*':
+                print("Error, should have avoided this!")
+                exit(1)
             else:
-                sub = p[pPass]
-                pPass += 1
-            if p[pPass] == '*':
+                sub = p[i]
+            if p[i] == '*':
                 #Should have marked the examined substring as passed already
-                pPass += 1
-                match = self.match_star(s[sPass:], sub, len(p) - (len(sub) + pPass))
-            else:
-                match = self.is_subMatch(s[sPass:], sub)
-            if match != -1:
-                sPass += match
-            else:
+                spass += self.match_star(s[spass:], sub, len(p) - len(sub))
+            elif p[i] != ('.' or s[spass]):
                 return False
-
-    def is_subMatch(self, s, p, sPass, pPass):
-
-        while sPass < (len(s) - 1) and pPass < (len(p) - 1):
-            if p[pPass] == '(':
-                self.is_match_parenthesis(s, p, sPass, pPass+1)
+            else: spass += 1
+            if spass == len(s): return True
 
     def get_substr(self, p):
         start = self.pPass
@@ -45,11 +41,15 @@ class RegEx:
         return None
 
     def match_star(self, s, sub, cut):
-        stars = 0
+        str = 0
         footer = cut + ((len(s) - cut) % len(sub))
         for i in range(len(s) - cut, len(sub)):
-            stars += 1 if self.isMatch(s[i:footer], sub) else 0
-            #Going to have to test this for any off by ones
-        return stars
+            if self.isMatch(s[i:footer], sub):
+                str += 1
+            else:
+                return str
+        return str
+
+    main()
 
 
